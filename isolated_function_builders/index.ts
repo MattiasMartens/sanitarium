70ce893d55dea11ee95c6e45b25befd5d26b0eb8
Mattias Martens <mattiasofmartens@gmail.com>
2019-09-18 17:24:17 -0700

    export

--- isolated_function_builders/index.ts
+++ isolated_function_builders/index.ts
@@ -4,6 +4,7 @@ export * from "./forked-functions";
 export * from "./guarded-functions";
 export * from "./injected-functions";
 export * from "./fiat-functions";
+export * from "./provider-functions";
 
 export type BaseFunction<ArgType extends any[], ReturnType> = (...args: ArgType) => ReturnType;
 export type BaseFunctionAsync<ArgType extends any[], ReturnType> = (...args: ArgType) => Promise<ReturnType>;
